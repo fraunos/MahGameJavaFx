e374974202ffe31be465caefcba16ad41880be39
fraunos <fraunos@gmail.com>
2015-02-14 21:57:48 +0100

    still testing

--- src/pl/fraunos/fxgame/Main.java
+++ src/pl/fraunos/fxgame/Main.java
@@ -1,4 +1,7 @@
 package pl.fraunos.fxgame;
+
+import java.util.Random;
+
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -8,47 +11,65 @@ import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
 import javafx.scene.media.AudioClip;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class Main extends Application {
-	Group group;
-	Player player;
-	final int sizeX = 200;
-	final int sizeY = 200;
-	static AudioClip ac;
-	
-	Timeline time = new Timeline(60, new KeyFrame(Duration.millis(1000 / 60), e -> {
-		player.update();
-	}));
+    static Random random;
+    static Scene scene;
+    static Group group;
+    static Player player;
+    static Circle circle;
+    final static int sizeX = 200;
+    final static int sizeY = 200;
+    static AudioClip ac;
 
-	public static void main(String[] args) {
-		ac = new AudioClip(Main.class.getResource("res/dobra.mp3").toString());
-		launch(args);
-		
+    Timeline time = new Timeline(60, new KeyFrame(Duration.millis(1000 / 60), e -> {
+	updateWorld();
+    }));
+
+    public static void main(String[] args) {
+	ac = new AudioClip(Main.class.getResource("res/dobra.mp3").toString());
+	launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+	random = new Random();
+	circle = new Circle(random.nextDouble() * sizeX, random.nextDouble() * sizeY, 15);
+	player = new Player(sizeX / 2, sizeY / 2);
+	group = new Group(player, circle);
+	scene = new Scene(group, 400, 400, true, SceneAntialiasing.DISABLED);
+	scene.setFill(Color.GRAY);
+	time.setCycleCount(Animation.INDEFINITE);
+	scene.setOnKeyPressed(e -> {
+	    player.startMoving(e.getCode());
+	    System.out.println(e.getCode());
+	});
+	scene.setOnKeyReleased(e -> {
+	    player.isMoving = false;
+	    System.out.println(e.toString());
+	});
+	time.play();
+	primaryStage.setResizable(false);
+	primaryStage.setScene(scene);
+	primaryStage.show();
+    }
+
+    public static boolean checkCollision(Player player, Circle circle) {
+	double totalRadius = player.getRadius() + circle.getRadius();
+	double distance = Math.hypot(player.getCenterX() - circle.getCenterX(), player.getCenterY() - circle.getCenterY());
+	return distance < totalRadius;
+    }
+
+    public static void updateWorld() {
+	player.update();
+
+	if (checkCollision(player, circle)) {
+	    circle.setCenterX(random.nextDouble() * sizeX);
+	    circle.setCenterY(random.nextDouble() * sizeY);
 	}
-
-	@Override
-	public void start(Stage primaryStage) {
-
-		player = new Player(sizeX / 2, sizeY / 2);
-		group = new Group(player);
-		Scene scene = new Scene(group, 400, 400, true, SceneAntialiasing.DISABLED);
-		scene.setFill(Color.GRAY);
-		time.setCycleCount(Animation.INDEFINITE);
-		scene.setOnKeyTyped(e -> {
-			player.startMoving(e.getCode());
-			System.out.println(e.toString());
-			System.out.println(ac.volumeProperty().get());
-		});
-		scene.setOnKeyReleased(e -> {
-			player.isMoving = false;
-			System.out.println(e.toString());
-			ac.play();
-		});
-		time.play();
-		primaryStage.setResizable(false);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+    }
 }
